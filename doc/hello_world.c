64b22f2b35820e3f2e14c7340515da204b8c5345
Muehlbauer, Thomas <thomas.muehlbauer@magna.com>
2026-02-17 08:42:45 +0100

    Add Ivan + Satish

--- doc/hello_world.c
+++ doc/hello_world.c
@@ -1,8 +1,8 @@
-#include <stdio.h>
+// #include <stdio.h>
 
-void manipulate_pwm(int value)
+void manipulate_pwm(int num)
 {
-    value++;
+    num++;
 }
 
 int main() 
@@ -10,8 +10,8 @@ int main()
     int num = 5;
 
     manipulate_pwm(num);
-    printf("Value: %d\n", num);
+    // printf("Value: %d\n", num);
 
-    printf("Hello, World!\n");
-    return 0;
+    // printf("Hello, World!\n");
+    return num;
 }
